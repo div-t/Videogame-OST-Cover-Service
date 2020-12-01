a38db076262a8afc0480c74c74c5cf51141d3806
Toni <7136@htl.rennweg.at>
2020-12-01 15:01:39 +0100

    The first commit, User Story 1

---
+++ OST.php
@@ -0,0 +1,9 @@
+<?php
+require_once("Song.php");
+
+abstract class OST {
+    private $uniqueID;
+    private $name;
+    private $releaseYear;
+    private $trackList;
+}
---
+++ Song.php
@@ -0,0 +1,8 @@
+<?php
+class Song {
+    private $uniqueID;
+    private $name;
+    private $artist;
+    private $trackNumber;
+    private $duration;
+}
