953071e3313e0a2a14665646a4889f15863b6312
Toni <7136@htl.rennweg.at>
2021-01-19 15:34:58 +0100

    The commit with Advanced 2. 19.01.2021

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('Seeder.php');
 
 $var = Seeder::getData();
 
-if (isset($_GET['format'])) {
+if (isset($_GET['format']) && preg_match("/[\w\.-]+$/",  $_GET['format'])) {
     if ($_GET['format'] == 'html') {
         header('Content-Type: text/html');
     } else {
@@ -16,9 +16,9 @@ if (isset($_GET['format'])) {
 
 if (isset($_GET['OSTID']) && isset($_GET['ALL'])) {
     echo 'Fatal Error: Both Parameters are set. You might try to use only one GET Parameter';
-} else if (isset($_GET['OSTID'])) {
+} else if (isset($_GET['OSTID']) && preg_match("/\d+$/",  $_GET['OSTID'])) {
     echo json_encode($var[$_GET['OSTID']]);
-} else if (isset($_GET['ALL'])) {
+} else if (isset($_GET['ALL']) && preg_match("/\w+$/",  $_GET['ALL'])) {
     if ($_GET['ALL'] == 'true') {
         echo json_encode($var);
     } else {
